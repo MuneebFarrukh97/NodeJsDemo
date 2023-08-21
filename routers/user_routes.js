b660b859418d145738a935990e3b96272ba2d0f6
MuneebFarrukh97 <58254985+MuneebFarrukh97@users.noreply.github.com>
2023-08-21 17:50:58 +0500

    Revert "DemoApp"
    This reverts commit e5b06ce8cbd26a014705ccebdbf351b7abeceffe.

--- routers/user_routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-const userController = require("../controller/user_controller");
-
-router.post('/registration',userController.register);
-router.post('/login',userController.login);
-
-module.exports = router;
