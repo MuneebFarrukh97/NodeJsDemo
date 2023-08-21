c3a380870fa1e4884e6f026271f18c4676a19a18
MuneebFarrukh97 <58254985+MuneebFarrukh97@users.noreply.github.com>
2023-08-21 17:51:55 +0500

    NodeJsDemo

---
+++ backend/routers/user_routes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+const userController = require("../controller/user_controller");
+
+router.post('/registration',userController.register);
+router.post('/login',userController.login);
+
+module.exports = router;
